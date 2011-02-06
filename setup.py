3b3c325447658269b1b9b25b2fe8cd9c50996a46
Gora Khargosh <gora.khargosh@gmail.com>
2011-02-06 18:24:19 +0530

    Version 0.1.0
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     long_description=open('README').read(),
     author='Gora Khargosh',
     author_email='gora.khargosh@gmail.com',
-    version="0.9.9",
+    version="0.1.0",
     url="http://pypi.python.org/pypi/squeeze",
     license='Public Domain',
     packages=find_packages(),
